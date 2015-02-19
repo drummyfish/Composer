961e3f6604ffefa9dc653d17d5b94cbbe7c1609f
drummyfish <tastyfish@seznam.cz>
2015-02-19 20:56:24 +0100

    parser

--- src/composer.py
+++ src/composer.py
@@ -1,3 +1,7 @@
+import re
+import random
+import itertools
+import collections
 
 # some MIDI instrument codes:
 
@@ -6,6 +10,103 @@ INSTRUMENT_GUITRAR = 25
 INSTRUMENT_EBASS = 36
 INSTRUMENT_ROCK_DRUMS = 255    # not an actual MIDI code, this is an internal value
 INSTRUMENT_STRINGS = 48
+
+## Flattens nested lists.
+
+flatten = lambda *n: (e for a in n
+  for e in (flatten(*a) if isinstance(a, (tuple, list)) else (a,)))
+
+## Serves for random generation of various things.
+
+class RandomGenerator:
+  ## Generates a composition structure (a sequence of section names) from
+  #  composition structure string (a regular expression-like string, see
+  #  the composition file format specification for details).
+  #
+  #  @param cs_string composition structure string (see the composition
+  #         file specification)
+  #  @param seed integer random seed
+  #  @return randomly generated composition structure as a list of
+  #          section names represented as strings
+
+  def generate_composition_structure(self, cs_string, seed):
+    # remove extra spaces:
+    cs_string = re.sub('\s+',' ',cs_string).lstrip().rstrip()
+    cs_string = re.sub('\s*\(\s*',' ( ',cs_string)
+    cs_string = re.sub('\s*\)\s*',' ) ',cs_string)
+    cs_string = re.sub('\s*\{\s*',' { ',cs_string)
+    cs_string = re.sub('\s*\}\s*',' } ',cs_string)
+    cs_string = re.sub('\s*\[\s*',' [ ',cs_string)
+    cs_string = re.sub('\s*\]\s*',' ] ',cs_string)
+    tokens = cs_string.split()
+
+    random.seed(seed)
+
+    return self.__parse_composition_structure(tokens,seed)
+
+  ## Private function that recursively parses the composition structure
+  #  string.
+  #
+  #  @param tokens list of string tokens representing the structure to
+  #         be parsed
+  #  @param seed seed for random generation
+  #  @return list of generated section names as strings
+
+  def __parse_composition_structure(self, tokens, seed):
+    def is_text(what):
+      return what not in ["(",")","[","]","{","}"]
+
+    square_brackets = False
+    result = []
+    helper_list = []
+
+    position = 0
+
+    while position < len(tokens):
+      if tokens[position] == "[":
+        square_brackets = True
+        helper_list = []
+      elif tokens[position] == "]":
+        result.append(random.choice(helper_list))
+        square_brackets = False
+      elif tokens[position] == "{":
+        helper_string = ""        # read the stuff between { and }
+        while True:
+          position += 1
+
+          if tokens[position] == "}":
+            break
+
+        # TODO: use helper_string here to generate random number and repeat previous section
+      else:                                   # ( or a section name
+        item = tokens[position]
+
+        if tokens[position] == "(":           # recurse
+          position2 = position
+          bracket_count = 0
+
+          while True:
+            if tokens[position2] == "(":
+              bracket_count += 1
+            elif tokens[position2] == ")":
+              bracket_count -= 1
+              if bracket_count == 0:
+                break
+
+            position2 += 1
+
+          item = self.__parse_composition_structure(tokens[position + 1:position2],seed)
+
+          position = position2
+
+        if square_brackets:
+            helper_list.append(item)
+        else:
+            result.append(item)
+
+      position += 1
+
+    return list(flatten(result))
 
 ## Represents a section, or more accurately a section definition
 #  (not a section instance), that can be loaded from the compositor
@@ -43,7 +144,7 @@ class SectionInstance:
     result = "       "
 
     for i in range(self.length_bars):
-      result += "...|"
+      result += "|..............."
 
     result += "\n"
 
@@ -71,7 +172,7 @@ class Note:
   def __init__(self, start, length, note, velocity):
     self.init_attributes()
     self.start = start
-    self.length = 0.25
+    self.length = length
     self.velocity = velocity
     self.note = note
 
@@ -145,7 +246,7 @@ class SectionTrack:
       nothing = True
 
       for note in self.notes:
-        if i > note.start and i < note.start + note.length:
+        if i >= note.start and i <= note.start + note.length:
           result += str(note)
           nothing = False
           break
@@ -153,7 +254,7 @@ class SectionTrack:
       if nothing:
         result += "."
 
-      i += 0.25
+      i += 0.0625
 
     return result
 
@@ -170,12 +271,21 @@ class Composition:
   def save_as_midi():
     return
 
-instance = SectionInstance()
-track = SectionTrack()
-track2 = SectionTrack()
-track2.instrument = INSTRUMENT_ROCK_DRUMS
-track.add_note(Note(0.1,1.2,120,100))
-track.add_note(Note(0.6,2.5,100,50))
-instance.add_track(track)
-instance.add_track(track2)
-print(instance)
+#instance = SectionInstance()
+#track = SectionTrack()
+#track2 = SectionTrack()
+#track2.instrument = INSTRUMENT_ROCK_DRUMS
+
+#track2.add_note(Note(0.5,1,120,100))
+#track2.add_note(Note(2.0,1,100,50))
+
+#track.add_note(Note(1,0.6,120,100))
+#track.add_note(Note(3.0,0.25,100,50))
+
+#instance.add_track(track)
+#instance.add_track(track2)
+#print(instance)
+
+r = RandomGenerator()
+print(r.generate_composition_structure(" aaaa [bbb ccccc dddddddd (XXXXXX YYYYYYY) ]{uniform(2,3)} (xxxx [(fufufu sesese) (popopo mumumu)]) eeeeeee{ uniform(2,3) } ffffff ",12315))
+#r.generate_composition_structure("    rock (   (rock_chorus   | pop_chorus)[  uniform(2,3) ]  |   rock_bridge[10])   (rock_chorus(pop_chorus) ) ",12314)
